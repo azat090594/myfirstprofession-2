9596481f12a6d6234b382d475ef4a3a1d8971afb
azat090594 <71090859+azat090594@users.noreply.github.com>
2020-09-15 23:30:43 +0400

    Update lesson1.js

--- js/lesson1.js
+++ js/lesson1.js
@@ -2,4 +2,4 @@ let r = prompt ("Enter value of r") ; /// get value from user
 const a = Math.PI
 let R = ( ( 2 * a ) * r ) ;
 let S = a * (r * r ) ;
-alert(" Perimeter of circle is " + R ,  "Area of circle is " + S + "." ) ;
+alert(" Perimeter of circle is " + R +  "Area of circle is " + S + "." ) ;
